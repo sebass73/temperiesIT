e1e526a38454d33006d1b8425a8a809dae6e2f52
Sebastián Martínez <smartinez@blitzit.com.ar>
2021-11-22 21:21:22 -0300

    Agrego dependencia thymeleaf y el endpoint por defecto

--- src/main/java/com/temperiesIt/sebastian/controller/rest/PersonaController.java
+++ src/main/java/com/temperiesIt/sebastian/controller/rest/PersonaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -20,6 +21,11 @@ import java.util.List;
 public class PersonaController {
     @Autowired
     private PersonaService personaService;
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
